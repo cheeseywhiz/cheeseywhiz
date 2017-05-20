e6d1b552d3f9ae7bd67e1b64f16666983bb0bf5c
cheeseywhiz <cheesy.mac.dott@gmail.com>
2017-05-20 00:27:35 -0400

    Doc strings and fix W503 on Anaconda

--- GUIs/Battery/graph.py
+++ GUIs/Battery/graph.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Provides continuous output of battery level data."""
 from matplotlib import pyplot as plt
 import battery
 
 
 def loop():
+    """pass."""
     pass
--- GUIs/Battery/main.py
+++ GUIs/Battery/main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Battery desktop widget.
+
+Displays graph of battery usage data on desktop."""
 import battery
 
 if __name__ == '__main__':
