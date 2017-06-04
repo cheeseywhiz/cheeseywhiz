97f63ccd3dde8ac0486679f21b2dee49bf523b93
Spencer Todd <cheesy.mac.dott@gmail.com>
2017-06-03 20:21:52 -0400

    Combine txt files from folder and make a wordcloud

--- Wordcloud/main.py
+++ Wordcloud/main.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+import os
+import sys
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+text = []
+dir = os.path.abspath(sys.argv[1])
+files = [dir + '/' + file
+         for file in os.listdir(dir)]
+
+for file in files:
+    with open(file) as f:
+        text.append(f.read())
+
+text = ' '.join(text)
+
+wordcloud = WordCloud().generate(text)
+
+image_args = [wordcloud, ]
+image_kwargs = {'interpolation': 'quadric', }
+plt.axis('off')
+plt.imshow(*image_args, **image_kwargs)
+plt.savefig('wordcloud.png')
+plt.show()
