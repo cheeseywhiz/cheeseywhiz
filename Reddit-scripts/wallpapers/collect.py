fb6798ff298bb406becc1a3f57c967c01e9c4738
Spencer Todd <cheesy.mac.dott@gmail.com>
2017-08-23 01:08:46 -0400

    Implement ping and reimplement logger

--- Reddit-scripts/wallpapers/collect.py
+++ Reddit-scripts/wallpapers/collect.py
@@ -1,34 +1,58 @@
 #!/home/cheese/cheeseywhiz/Reddit-scripts/wallpapers/bin/python
-import functools
-import random
-import requests
 import sys
 from collections import namedtuple as _namedtuple
+from functools import partial, wraps
 from pathlib import Path
+from random import shuffle
+from requests import get as _get
+from subprocess import Popen, DEVNULL
+from time import sleep
 from urllib.parse import urlparse
 
 REDDIT_LINK = 'https://www.reddit.com/r/earthporn/hot/.json?limit=10'
 
 
-def log(message):
-    print(f'{Path(__file__).name}: {message}', file=sys.stderr)
+def _rich_message(*values, beginning=None, label=None, **print_kwargs):
+    if beginning is None:
+        beginning = Path(__file__).name + ': '
+
+    print_func = partial(partial(print, flush=True), **print_kwargs)
+    print_label = partial(print_func, end='')
+
+    print_label(beginning)
+
+    if label is not None:
+        print_label(label)
+
+    print_func(*values)
 
 
-@functools.wraps(requests.get)
+log = partial(_rich_message, file=sys.stderr)
+error = partial(_rich_message, label='Error: ', file=sys.stderr)
+
+
+def ping(host):
+    return not Popen(
+        ['ping', '-c 1', '-w 1', host],
+        stdout=DEVNULL, stderr=DEVNULL,
+    ).wait()
+
+
+@wraps(_get)
 def get(*args, **kwargs):
-    log(f'Requesting {args[0]}')
-    res = functools.partial(
-        requests.get,
-        headers={'User-Agent': 'u/cheeseywhiz'}
+    log(f'Requesting {args[0]} ... ', end='')
+    res = partial(
+        _get,
+        headers={'User-Agent': 'u/cheeseywhiz'},
     )(*args, **kwargs)
-    log(f'{res.status_code} {res.reason}')
+    log(res.status_code, res.reason, beginning='')
     return res
 
 
 def _named_tuple_wrapper(func, type_name, field_names, *, verbose=False,
                          rename=False, module=None):
     """\
-    Wrap a function to turn its result into a named tuple.
+    Wrap a function by turning its result into a named tuple.
     """
     if type_name is None:
         type_name = f'{func.__name__}_result'
@@ -36,7 +60,7 @@ def _named_tuple_wrapper(func, type_name, field_names, *, verbose=False,
     result_tuple = _namedtuple(
         type_name, field_names, verbose=verbose, rename=rename, module=module)
 
-    @functools.wraps(func)
+    @wraps(func)
     def wrapped_func(*func_args, **func_kwargs):
         result = func(*func_args, **func_kwargs)
         return result_tuple._make(result)
@@ -51,7 +75,7 @@ def namedtuple(*field_names, type_name=None, verbose=False, rename=False,
     has a similar signature to collections.namedtuple but is more decorator
     friendly.
     """
-    return functools.partial(
+    return partial(
         _named_tuple_wrapper,
         type_name=type_name, field_names=field_names, verbose=verbose,
         rename=rename, module=module)
@@ -64,7 +88,7 @@ def random_map(func, *iterables):
         args = zip(*iterables)
 
     args = list(args)
-    random.shuffle(args)
+    shuffle(args)
 
     return map(func, *zip(*args))
 
@@ -93,6 +117,22 @@ def download(dir, url):
 
 
 def main(argv):
+    max_seconds = 60
+    seconds_wait = 5
+
+    for n_try in range(max_seconds // seconds_wait):
+        if not ping('8.8.8.8'):
+            error('Connection not found')
+            sleep(seconds_wait)
+        elif n_try:
+            log('Connection found')
+            break
+        else:
+            break
+    else:  # no break; really no internet connection
+        error('Too many tries')
+        return 1
+
     try:
         save_dir = Path(argv[1])
     except IndexError:
@@ -104,22 +144,23 @@ def main(argv):
         post['data']['url']
         for post in get(REDDIT_LINK).json()['data']['children'])
 
-    download_ = functools.partial(download, save_dir)
+    download_ = partial(download, save_dir)
 
     for res in random_map(download_, urls):
         if not res.succeeded:
-            log(f'Error: {res.status}: {res.url}')
+            error(res.status, res.url, sep=': ')
             continue
         else:
             log(res.status)
-            log(f'Output: {res.path}')
+            log(res.path, label='Output: ')
             print(res.path)
-            break
-    else:  # no break
+            return 0
+    else:  # no break; did not succeed
         return 1
-
-    return 0
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    main_return = main(sys.argv)
+
+    if main_return:
+        sys.exit(main_return)
