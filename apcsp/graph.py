53430f7e4c2507819207e443c3c4309a7f1fbab9
Spencer Todd <cheesy.mac.dott@gmail.com>
2018-04-25 21:38:59 -0400

    Smarter tex docs and variable name changes

--- apcsp/graph.py
+++ apcsp/graph.py
@@ -6,30 +6,26 @@ f(x,y,z)=z/sqrt(x**2+y**2)"""
 import sys
 import numpy as np
 import sympy
-import matplotlib
-matplotlib.use('Qt5Agg')
-
 from sympy.abc import x, y, z, C
-from matplotlib import cm
-from matplotlib import pyplot as plt
+from matplotlib import cm, pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def repeat(obj):
+def repeat(an_object):
     while True:
-        yield obj
+        yield an_object
 
 
-def prompt_expr():
+def prompt_expression():
     """Gather a mathematical expression from the user. Raises OSError on
     data/pipe mismatch.
 
-    $ ./graph.py  # Interactive prompt
-    f(x,y,z)=(z**2)/(x**2+y**2)
+    $ ./graph.py  # Pass expression via interactive prompt
+    f(x,y,z)=z/sqrt(x**2+y**2)
 
-    $ ./graph.py "(z**2)/(x**2+y**2)"  # Pass expression as parameter
+    $ ./graph.py "z/sqrt(x**2+y**2)"  # Pass expression as parameter
 
-    $ echo "(z**2)/(x**2+y**2)" | ./graph.py -  # Pass expression over pipe"""
+    $ echo "z/sqrt(x**2+y**2)" | ./graph.py -  # Pass expression over pipe"""
     if len(sys.argv) > 1:
         if sys.argv[1] == '-':
             if sys.stdin.isatty():
@@ -44,47 +40,47 @@ def prompt_expr():
         raise OSError('pipe with no \'-\' flag')
 
 
-def eval_str(str_expr):
+def eval_expression(expression: str):
     """Evaluate a string expression into a sympy expression. Available
     namespace is that of sympy.* and x, y, and z variables."""
-    return eval(str_expr, vars(sympy), {'x': x, 'y': y, 'z': z})
+    return eval(expression, vars(sympy), {'x': x, 'y': y, 'z': z})
 
 
-def solve_z(sympy_expr):
+def solve_z(expression: sympy.Expr):
     """Solve C=f(x,y,z) for z for some sympy expression f(x,y,z). Returns a
     list of expressions with variables x, y, and C."""
-    return sympy.solve(sympy.Eq(C, sympy_expr), z)
+    return sympy.solve(sympy.Eq(C, expression), z)
 
 
-def solve_z_str(str_expr):  # OVAL
+def solve_z_str(expression: str):
     """Solve C=f(x,y,z) for z for some string expression f(x,y,z)."""
-    return solve_z(eval_str(str_expr))
+    return solve_z(eval_expression(expression))
 
 
 def solve_z_prompt():
     """Solve C=f(x,y,z) for z for some expression f(x,y,z) that is prompted
     for to the user."""
-    return solve_z_str(prompt_expr())
+    return solve_z_str(prompt_expression())
 
 
-def expr_to_f_xy(sympy_expr, constant):
+def make_lambda(expression: sympy.Expr, constant):
     """Form a function of two variables from the sympy expression while
     substituting the arbitrary constant C."""
-    return sympy.Lambda((x, y), sympy_expr.subs(C, constant))
+    return sympy.Lambda((x, y), expression.subs(C, constant))
 
 
 # named after enumerate() but for colors
 def e_color_ate(constants):
     """Generate corresponding colors for each constant. Yields
     color, constant."""
-    d = len(constants) - 1
+    index_max = len(constants) - 1
 
-    for n, constant in enumerate(constants):
-        color = cm.jet(n / d, 0.93)
+    for index, constant in enumerate(constants):
+        color = cm.jet(index / index_max, 0.93)
         yield color, constant
 
 
-def map_f_xy(f, mesh):  # RECTANGLE
+def map_f_xy(f, mesh):
     """Map a numpy (x,y) meshgrid to a function of two variables."""
     domain = np.array(mesh).transpose(1, 2, 0)
     z_range = np.empty(mesh[0].shape)
@@ -101,22 +97,22 @@ class LevelSurfacePlotter(Axes3D):
     """Plot functions of two variables or level curves of functions of three
     variables."""
 
-    def plot_map_f_xy(self, f, mesh_domain, **kwargs):
+    def plot_f_xy(self, f, mesh_domain, **kwargs):
         """Plot f(x,y) in three dimensions whereas mesh_domain is a numpy
         meshgrid of (x,y) coordinates."""
         super().plot_surface(
             *mesh_domain, map_f_xy(f, mesh_domain),
             **kwargs)
 
-    def plot_map_f_xyz(
-            self, sympy_expr_list, x_limits, y_limits, C_limits, **kwargs
+    def plot_level_surfaces(
+            self, solutions: list, x_limits, y_limits, C_limits,
+            **kwargs
     ):
-        """Plot f(x,y,z) set equal to various constants and solved for
-        z=g(x,y) whereas *_limits is a list of possible values for each
-        variable."""
+        """Plot a number of expressions that contain x, y, and C. The
+        parameters *_limits are lists of possible values."""
         for color, constant in e_color_ate(C_limits):
-            for f_xy in map(expr_to_f_xy, sympy_expr_list, repeat(constant)):
-                self.plot_map_f_xy(
+            for f_xy in map(make_lambda, solutions, repeat(constant)):
+                self.plot_f_xy(
                     f_xy, np.meshgrid(x_limits, y_limits),
                     color=color, **kwargs)
 
@@ -130,7 +126,7 @@ def main():
     C_limits = list(np.arange(100, -100 - 10, -20))
     C_limits.remove(0)
 
-    ax.plot_map_f_xyz(solve_z_prompt(), x_limits, y_limits, C_limits)
+    ax.plot_level_surfaces(solve_z_prompt(), x_limits, y_limits, C_limits)
     plt.show()
 
 
