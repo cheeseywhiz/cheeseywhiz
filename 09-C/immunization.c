41731a4d4eb1122df80ad56b30f6876980c7058f
Spencer Todd <cheesy.mac.dott@gmail.com>
2018-11-11 21:58:24 -0500

    Put csv data into 2D linked list

--- 09-C/immunization.c
+++ 09-C/immunization.c
@@ -10,6 +10,74 @@
 
 #define LOG_ERRNO(function) fprintf(stderr, "%s: %s\n", function, strerror(errno))
 
+struct ll_node* ll_new(void *data) {
+        struct ll_node *node = malloc(sizeof(struct ll_node));
+
+        if (node == NULL) {
+                LOG_ERRNO("malloc");
+                return NULL;
+        }
+
+        node->next = NULL;
+        node->data = data;
+        return node;
+}
+
+struct ll_node* ll_end(struct ll_node **head) {
+        if (*head == NULL) {
+                return NULL;
+        }
+
+        struct ll_node *end;
+        for (end = *head; end->next != NULL; end = end->next);
+        return end;
+}
+
+struct ll_node* ll_append(struct ll_node **head, void *data) {
+        struct ll_node *new = ll_new(data);
+
+        if (!new) {
+                return NULL;
+        } else if (*head == NULL) {
+                *head = new;
+        } else {
+                struct ll_node *end = ll_end(head);
+                end->next = new;
+        }
+
+        return new;
+}
+
+void ll_deep_free(struct ll_node **head, ll_free_func free_data) {
+        struct ll_node *item, *item_alias;
+
+        for (item = *head; item != NULL; ) {
+                item_alias = item;
+                item = item->next;
+                free_data(item_alias->data);
+                free(item_alias);
+        }
+}
+
+void ll_2d_deep_free(struct ll_node **head, ll_free_func free_data) {
+        struct ll_node *row_ptr, *row_alias;
+
+        for (row_ptr = *head; row_ptr != NULL; ) {
+                row_alias = row_ptr;
+                row_ptr = row_ptr->next;
+                struct ll_node *cell_ptr, *cell_alias;
+
+                for (cell_ptr = row_alias->data; cell_ptr != NULL; ) {
+                        cell_alias = cell_ptr;
+                        cell_ptr = cell_ptr->next;
+                        free_data(cell_alias->data);
+                        free(cell_alias);
+                }
+
+                free(row_alias);
+        }
+}
+
 char* read_file(char *path) {
         struct stat st;
 
@@ -39,32 +107,46 @@ char* read_file(char *path) {
         return buffer;
 }
 
+void csv_free(struct ll_node *csv_data) {
+        ll_2d_deep_free(&csv_data, free);
+}
+
+static struct ll_node* ll_csv_append(struct ll_node **head, char *contents, int current_word) {
+        char *word = contents + current_word;
+        char *word_copy = malloc(strlen(word) + 1);
+        strcpy(word_copy, word);
+
+        if (word_copy == NULL) {
+                LOG_ERRNO("malloc");
+                return NULL;
+        }
+
+        return ll_append(head, word_copy);
+}
+
 #define HANDLE_SEP() \
         contents[ptr] = '\0'; \
-        printf("%d %d %d %s\n", row_n, col_n, ptr, contents + current_word); \
-        current_word = ptr + 1; \
-        col_n++
+        if (ll_csv_append(&row, contents, current_word) == NULL) { \
+                free(contents); \
+                ll_deep_free(&row, free); \
+                csv_free(csv_data); \
+                return NULL; \
+        } \
+        current_word = ptr + 1
 
-int read_csv(char *path) {
+struct ll_node* read_csv(char *path) {
         char *contents = read_file(path);
 
         if (!contents) {
-                return 1;
+                return NULL;
         }
 
-        int rows = 1;
-        int cols = 0;
+        struct ll_node *csv_data = NULL;
         int ptr;
-        for (ptr = 0; contents[ptr] != '\n'; ptr++) if (contents[ptr] == ',') rows++;
-        for (ptr = 0; contents[ptr] != '\0'; ptr++) if (contents[ptr] == '\n') cols++;
-
         int current_word = 0;
-        int row_n = 0;
-        int col_n;
 
         for (ptr = 0; contents[ptr] != '\0'; ptr++) {
-                printf("before\n");
-                col_n = 0;
+                struct ll_node *row = NULL;
 
                 for (;; ptr++) {
                         if (contents[ptr] == '\n') {
@@ -75,15 +157,37 @@ int read_csv(char *path) {
                         }
                 }
 
-                row_n++;
-                printf("after\n");
+                if (ll_append(&csv_data, row) == NULL) {
+                        free(contents);
+                        ll_deep_free(&row, free);
+                        csv_free(csv_data);
+                        return NULL;
+                }
         }
 
         free(contents);
-        return 0;
+        return csv_data;
+}
+
+void print_csv(struct ll_node **csv_data) {
+        struct ll_node *row_ptr;
+        struct ll_node *cell_ptr;
+
+        for (row_ptr = *csv_data; row_ptr != NULL; row_ptr = row_ptr->next) {
+                for (cell_ptr = row_ptr->data; cell_ptr != NULL; cell_ptr = cell_ptr->next) {
+                        char *data = cell_ptr->data;
+                        fputs(data, stdout);
+                        if (cell_ptr->next) putchar(',');
+                }
+
+                putchar('\n');
+        }
 }
 
 int main(int argc, char *argv[]) {
-        read_csv("immunization.csv");
+        struct ll_node *csv_data = read_csv("immunization.csv");
+        if (csv_data == NULL) return 1;
+        print_csv(&csv_data);
+        csv_free(csv_data);
         return 0;
 }
--- 09-C/immunization.h
+++ 09-C/immunization.h
@@ -1,6 +1,19 @@
 #ifndef IMMUNIZATION_H
 #define IMMUNIZATION_H
 
+struct ll_node {
+        struct ll_node *next;
+        void *data;
+};
+
+typedef void (*ll_free_func)(void *ptr);
+
+struct ll_node* ll_new(void *data);
+struct ll_node* ll_end(struct ll_node **head);
+struct ll_node* ll_append(struct ll_node **head, void *data);
+void ll_deep_free(struct ll_node **head, ll_free_func free_data);
+void ll_2d_deep_free(struct ll_node **head, ll_free_func free_data);
+
 struct immunization_data {
         char *year_label;
         char **years;
@@ -15,6 +28,8 @@ struct immunization_data {
 };
 
 char* read_file(char *path);
-int read_csv(char *path);
+void csv_free(struct ll_node *csv_data);
+struct ll_node* read_csv(char *path);
+void print_csv(struct ll_node **csv_data);
 
 #endif
