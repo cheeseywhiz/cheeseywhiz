a48513000c28f52c2053655fd9f50ee2b19104e2
Spencer Todd <cheesy.mac.dott@gmail.com>
2024-09-02 00:51:52 -0500

    Finish album matcher POC

--- sift/album_match.py
+++ sift/album_match.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 # https://docs.opencv.org/4.x/dc/dc3/tutorial_py_matcher.html
 import os
+import pdb
 import sys
 import time
 import cv2 as cv
 import numpy as np
 from pprint import pprint as pp
 from statistics import mean
+import functools
+
+DEBUG = False
+IM_WIDTH = 900
 
 
 def imshow(img):
@@ -16,50 +21,83 @@ def imshow(img):
     cv.destroyAllWindows()
 
 
-def imread(fname):
+def imread(fname, resize_width=None):
     img = cv.imread(fname)
+
+    if resize_width is not None:
+        (h, w) = img.shape[:2]
+        aspect_ratio = h / w
+        new_height = int(resize_width * aspect_ratio)
+        img = cv.resize(img, (resize_width, new_height))
+
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     return img, gray
 
 
-def get_library(folder, feature_detector):
+def get_library(folder, feature_detector, resize_width=None):
     library = {} # fname: (img, gray, keypoints, descriptor)
 
     for fname in os.listdir(folder):
-        img, gray = imread(f'{folder}/{fname}')
+        img, gray = imread(f'{folder}/{fname}', resize_width)
         keypoints, descriptor = feature_detector.detectAndCompute(gray, None)
         library[fname] = img, gray, keypoints, descriptor
 
     return library
 
 
+def debug(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception as e:
+            print(f'Exception: {e!r}')
+            pdb.post_mortem(sys.exc_info()[2])
+            raise
+
+    return wrapper
+
+
+@debug
 def main(argv):
+    global DEBUG
+    if '--debug' in argv:
+        DEBUG = True
+
     sift = cv.SIFT_create()
 
     library = get_library('album-covers-original', sift)
-    queries = get_library('queries', sift)
+    # assume query album will take up about 2/3 of the query picture
+    queries = get_library('queries', sift, resize_width=900 * 3 // 2)
 
     for query_fname in queries:
-        do_query(sift, library, queries, query_fname)
+        query_image(sift, library, queries, query_fname)
 
     pp(queries)
 
 
-def do_query(orb, library, queries, query_fname):
+def query_image(orb, library, queries, query_fname):
     q_img, q_gray, q_kp, query_descriptor = queries[query_fname]
     all_matches = {}
 
-    for library_fname, (_, _, _, library_descriptor) in library.items():
+    for library_fname, (l_img, _, l_kp, library_descriptor) in library.items():
         matcher = cv.BFMatcher()
         matches = matcher.knnMatch(library_descriptor, query_descriptor, k=2)
-        matches = mean(
-            m.distance
+        matches = [
+            [m]
             for m,n in matches
             if m.distance < 0.75 * n.distance
-        )
-        all_matches[library_fname] = matches
+        ]
 
-    queries[query_fname] = q_img, q_gray, query_descriptor, all_matches
+        if DEBUG:
+            matches_img = cv.drawMatchesKnn(l_img, l_kp, q_img, q_kp, matches, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+            imshow(matches_img)
+
+        matches_stat = len(matches)
+        all_matches[library_fname] = matches_stat
+
+    winner = sorted(all_matches.items(), key=lambda p: p[1])[-1] if library else None
+    queries[query_fname] = winner, all_matches
 
 
 if __name__ == '__main__':
